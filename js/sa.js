660c6d847a276e86be7e59c8554375f320815dab
Sunyou Kim <sunyou.kim@lge.com>
2017-11-21 10:00:26 +0900

    aaa

--- js/sa.js
+++ js/sa.js
@@ -28,12 +28,12 @@ function isMobile() {
 
 $(document).ready(function()
 {
+  $('body, html').css('height',screen.height + 'px');
+  $('body, html').css('min-height',screen.height + 'px');
   setTimeout(function() {
     $('.main-background').animate({backgroundColor:'rgba(0,0,0,0)'}, 2000, 'linear');
     $('.main-flower').animate({opacity: 0}, 2000, 'linear');
     $('.main-calendar, .main-name').animate({color:'rgba(0,0,0,1)'}, 2000, 'linear');
-    $('body, html').css('height',screen.height + 'px');
-    $('body, html').css('min-height',screen.height + 'px');
   }, 1000);
   if (isMobile()) {
     // $(window).scroll(function() {
