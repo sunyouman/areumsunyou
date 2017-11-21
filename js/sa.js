388e780ae54250380e950efb904ccc6b8ffd1438
Sunyou Kim <sunyou.kim@lge.com>
2017-11-21 15:57:42 +0900

    aaa

--- js/sa.js
+++ js/sa.js
@@ -100,6 +100,11 @@ function setPhotoGrid() {
 		var object = this;
 		if ($(this).hasClass('photo-grid-item-big') === true) {
 			$(this).removeClass('photo-grid-item-big');
+			$('.photo-grid').one('layoutComplete', function(e, item) {
+				$("html, body").animate({
+					scrollTop: $(document).scrollTop() + (screen.height/2) - ($(this).height()/2)
+				});
+			});
 		} else {
 			$('.photo-grid-item').removeClass('photo-grid-item-big');
 			$(this).addClass('photo-grid-item-big');
